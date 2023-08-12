64cc19870705a26c9adc7f37842f46fd9f544752
thexovc <osazeepeter79@gmail.com>
2023-08-12 18:54:03 +0100

    changed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Daniel Osariemen</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I build APIs, design frontends, <br className="sm:block hidden" />
+            I build APIs, engineer frontends, <br className="sm:block hidden" />
             and write smart contracts.
           </p>
 
